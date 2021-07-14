e0f498b8fa36b5b395d3d42d83a6eff5a363f4a7
RobbFreitas <robertocompromissos@gmail.com>
2021-07-14 11:41:34 -0300

    adicionada funcao p/ mostrar elementos contem bug

--- script.js
+++ script.js
@@ -3,13 +3,16 @@ var arr_tarefas = JSON.parse(localStorage.getItem('arr_tarefas') || '[]');
 function adicionar(){
 
     // Inserir elementos no array e no LocalStorage
-    var tarefa = document.getElementById("tarefa").value;
+    let tarefa = document.getElementById("tarefa").value;
     arr_tarefas.push(tarefa);
     localStorage.setItem("tarefa", arr_tarefas);
     
-    // Exibir os dados
-    document.getElementById('tarefas', tarefa);
-    localStorage.getItem('tarefas');
-    document.getElementById('lista').innerHTML = tarefa;
+    udpateScreen();
+}
 
+function udpateScreen(){
+
+    let takeTasks = (localStorage.getItem("tarefa", arr_tarefas));
+    console.log(localStorage.getItem("tarefa"));
+    document.getElementById("lista").innerHTML = takeTasks;
 }

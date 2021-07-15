b353e788ec61ca7bc49791c55065ff432835d0d6
RobbFreitas <robertocompromissos@gmail.com>
2021-07-14 21:14:09 -0300

    update

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Array para adicionar as tarefas
 var arr_tarefas = JSON.parse(localStorage.getItem('arr_tarefas') || '[]');
 
 function adicionar(){
@@ -5,7 +6,6 @@ function adicionar(){
     // Inserir elementos no array e no LocalStorage
     let tarefa = document.getElementById("tarefa").value;
     arr_tarefas.push(tarefa);
-
     localStorage.setItem("tarefa", JSON.stringify(arr_tarefas));
     
     udpateScreen();

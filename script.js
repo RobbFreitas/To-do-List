db57288334268d5ee1a025f2a3d73013b6e17653
RobbFreitas <robertocompromissos@gmail.com>
2021-07-15 11:36:56 -0300

    update

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 // Array para adicionar as tarefas
-var arr_tarefas = JSON.parse(localStorage.getItem('tasks')) || [];
+var arr_tarefas = JSON.parse(localStorage.getItem('tarefa')) || [];
 
 function adicionar(){
 
     // Inserir elementos no array e no LocalStorage
     let tarefa = document.getElementById("tarefa").value;
     arr_tarefas.push(tarefa);
-    localStorage.setItem("tasks", JSON.stringify(arr_tarefas));
-    
+    var taskList = localStorage.setItem("tarefa", JSON.stringify(arr_tarefas));
+
     udpateScreen();
 }
 
 function udpateScreen(){
 
-    let takeTasks = JSON.parse(localStorage.getItem("tasks"));
+    document.getElementById("lista").innerHTML = "";
+    localStorage.getItem("tarefa", arr_tarefas);
+    arr_tarefas.forEach((elemento) => {    
+        // Aqui eu concateno um li, com o elemento for da vez, um a um, dentro dessa template, 
+        // com <li> abraçando    
+        document.getElementById("lista").innerHTML += `<li>${elemento}</li>`;  
+    });
     
-    JSON.stringify(takeTasks);
-    document.getElementById("lista").innerHTML = takeTasks;
 }
 
